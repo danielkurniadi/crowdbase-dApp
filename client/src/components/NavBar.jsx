bb7b07ec57e901c0243bf806432d12df3956808f
iqdf <daniel.thekurniadi@gmail.com>
2023-10-25 11:40:05 +0700

    [client-fe] integration w thirdweb connect wallet
    * Allow coinbase, metamask, wallet to sign and connect
    
    * Impl "Connect Wallet" button (navbar) that pops up modal to connect.

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,67 +1,13 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import {
-  iconUrlSearch,
-  iconUrlThirdweb,
-} from "../assets";
-import {
-  ThirdwebProvider,
-  ConnectWallet,
-  metamaskWallet,
-  coinbaseWallet,
-  embeddedWallet,
-  trustWallet,
-  frameWallet,
-  darkTheme,
-} from "@thirdweb-dev/react";
-import CustomButton from "./CustomButton";
-import routeLinks from "../routes";
-
-const NavBarSmallScreenIcon = ({
-  name,
-  imgUrl,
-  handleClick = () => {},
-  isActive = false,
-  styles = "",
-}) => {
-  const backgroundColor = isActive ? "bg-[#2c2f32]" : "";
-  const grayscale = isActive ? "grayscale-0" : "grayscale";
-  const textColor = isActive ? "text-[#1dc071]" : "text-[#808191]";
-  <li
-    key={name}
-    className={`flex p-4 ${backgroundColor} ${styles}`}
-    onClick={handleClick}
-  >
-    <img
-      src={imgUrl}
-      alt={name}
-      className={`w-[24px] h-[24px] object-contain ${grayscale}`}
-    />
-    <p
-      className={`ml-[2opx] font-epilogue font-semibold text-[14px] ${textColor}`}
-    >
-      {name}
-    </p>
-  </li>;
-};
+import React from "react";
+import { iconUrlSearch } from "../assets";
+import { ConnectWallet } from "@thirdweb-dev/react";
 
 const NavBar = () => {
-  const navigate = useNavigate();
-  const [activeLink, setActiveLink] = useState(routeLinks.explore.name);
-  const [toggleDrawer, setToggleDrawer] = useState(false);
-  const navLinks = [
-    // NOTE: this is the order of the icons in the sidebar
-    routeLinks.explore,
-    routeLinks.campaigns,
-    routeLinks.payments,
-    routeLinks.profile,
-  ];
-
   return (
-    <div className="flex sm:flex-row justify-between mb-[35px] gap-6">
+    <div className="flex sm:flex-row justify-between mt-1 mb-[35px] pr-2 gap-6">
       <div
         id="navSearchBar"
-        className="flex md:flex-1 flex-row min-w-[150px] max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]"
+        className="flex md:flex-1 flex-row min-w-[150px] max-w-[458px] pl-4 pr-2 h-[35px] bg-[#1c1c24] rounded-[100px]"
       >
         <input
           type="text"
@@ -77,21 +23,10 @@ const NavBar = () => {
         </div>
       </div>
       <div className="flex flex-row justify-end gap-4">
-        <CustomButton
-          type="button"
-          title="Start your campaign"
-          styles="w-[200px] bg-[#4acd8d] text-[16px] mr-4"
-          handleClick={() => navigate(routeLinks.campaigns.path)}
+        <ConnectWallet
+          switchToActiveChain={true}
+          modalSize={"wide"}
         />
-        {/* TODO P2: Remove profile and redesign navbar */}
-        <Link to={routeLinks.profile.path}>
-          <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32]">
-            <img
-              src={iconUrlThirdweb}
-              className="w-[80%] h-[80%] object-contain"
-            />
-          </div>
-        </Link>
       </div>
     </div>
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import {
+  ThirdwebProvider,
+  metamaskWallet,
+  coinbaseWallet,
+  localWallet,
+  embeddedWallet,
+  trustWallet,
+  frameWallet,
+  darkTheme,
+} from "@thirdweb-dev/react";
 
-import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
-import App from './App';
-import './index.css';
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <ThirdwebProvider desiredChainId={ChainId.Goerli}>
+  <ThirdwebProvider
+    activeChain="base-goerli"
+    clientId="0e25b9112d89e2ae956459e44869d831"
+    supportedWallets={[
+      metamaskWallet(),
+      coinbaseWallet({ recommended: true }),
+      localWallet(),
+      embeddedWallet(),
+      trustWallet(),
+      frameWallet(),
+    ]}
+  >
     <Router>
-        <App />
+      <App />
     </Router>
   </ThirdwebProvider>
 );
